d96ed4d130bd1a5e29de3c682d1c8fdbb0719fb0
Juan Jose Jimenez <jjsanchez689@gmail.com>
2020-08-24 15:23:32 -0500

    add page home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
 
+const { Storage } = Plugins;
+
+// services
+import { ComicsService } from '../services/comics.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +12,50 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  listComics:any;
+
+  constructor(private comicsService: ComicsService) {}
+
+  ionViewWillEnter() {
+    this.getComics();
+  }
+
+  async clear() {
+    await Storage.clear();
+  }
+
+  async getComics() {
+    const comicsStorage = await this.getItemStorage('comics');
+    
+    // validate data storage
+    if(!comicsStorage) {
+      const comics = await this.comicsService.getComics();
+
+      for (let i = 0; i < comics.data.results.length; i++) {
+        comics.data.results[i].like = 0;
+        comics.data.results[i].dislike = 0;
+      }
+      await this.setItemStorage('comics',JSON.stringify(comics.data.results));
+    }
+
+    this.listComics = JSON.parse(comicsStorage);
+  }
+
+  async setItemStorage(key, value) {
+    await Storage.set({
+      key: key,
+      value: value
+    });
+  }
+
+  async getItemStorage(key) {
+    const { value } = await Storage.get({ key: key });
+    return value;
+  }
+
+  // receives the event index and type (like - dislike)
+  async eventButtonClick(){
+    await this.setItemStorage('comics',JSON.stringify(this.listComics));
+  }
 
 }
