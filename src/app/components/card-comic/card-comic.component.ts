831ab5d22c21c5119bb981b1a8c6667393a61bf2
Juan Jose Jimenez <jjsanchez689@gmail.com>
2020-08-24 15:22:37 -0500

    add logic to component card-comic

--- src/app/components/card-comic/card-comic.component.ts
+++ src/app/components/card-comic/card-comic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-card-comic',
@@ -7,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComicComponent implements OnInit {
 
+  @Input() comic: any;
+  @Input() index: any;
+
+  // emit event button (click)
+  @Output() eventButtonClick = new EventEmitter<any>();
+
+
   constructor() { }
 
   ngOnInit() {}
 
+  upLike() {
+    this.comic.like++;
+    this.eventButtonClick.emit();
+  }
+
+  upDisLike() {
+    this.comic.dislike++;
+    this.eventButtonClick.emit();
+  }
 }
